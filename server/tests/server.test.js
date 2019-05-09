5c2083bcda71fb21fc01d42ae494f7b6c7d484c3
Kapdoskar <kapdoskA@travelex.com>
2019-05-09 18:45:01 +0530

    Test Todos api using expect supertest mocha

---
+++ server/tests/server.test.js
@@ -0,0 +1,44 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {
+    app
+} = require('./../server');
+const {
+    Todo
+} = require('./../models/todo');
+
+beforeEach((done) => {
+    Todo.remove({}).then(() => done());
+});
+
+describe('POST /Todos', () => {
+    it('should create a new todo', (done) => {
+        var text = 'Test todo Text';
+
+        request(app).post('/todos').send({
+            text
+        }).expect(200).expect((res) => {
+            expect(res.body.text).toBe(text);
+        }).end((err, res) => {
+            if (err) return done(err);
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(1);
+                expect(todos[0].text).toBe(text);
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+
+    it('should not crete todo with invalid body data', (done) => {
+        request(app).post('/todos').send({}).expect(400).expect((res) => {
+            expect(res.body._message).toBe('Todo validation failed');
+        }).end((err, res) => {
+            if (err) return done(err);
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(0);
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+})
